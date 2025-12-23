b512806047ab6d1f5c725a0277c202a7e2ef7f1e
Tyler Johnson <tyler@netgizmo.net>
2025-12-23 13:35:20 -0600

    updated query in find-unvaulted-primaries

--- bin/find-unvaulted-primaries.bash
+++ bin/find-unvaulted-primaries.bash
@@ -17,6 +17,7 @@ VAULT_BASE="${1:-/home/tyler/packrat/vault/secret}"
 DB_HOST=${PGHOST:-cooper}
 DB_NAME=${PGDATABASE:-tyler}
 DB_USER=${PGUSER:-tyler}
+DB_PWD=${PGPASSWPRD:-cometdog}
 
 # SQL Logic:
 # 1. Get all hashes that have at least one copy in the vault.
@@ -26,24 +27,36 @@ DB_USER=${PGUSER:-tyler}
 SQL_QUERY=$(cat <<EOF
 WITH vault_hashes AS (
     SELECT DISTINCT hash
-    FROM images
+    FROM files
     WHERE base_path = '$VAULT_BASE'
 )
 SELECT
     ip.hash,
-    ip.full_path AS primary_location,
-    ip.last_modified AS primary_time,
-    i_vault.full_path AS vault_copy_location,
-    i_vault.disposition AS vault_copy_status
-FROM images_primary ip
-JOIN vault_hashes vh ON ip.hash = vh.hash
-JOIN images i_vault ON i_vault.hash = vh.hash AND i_vault.base_path = '$VAULT_BASE'
-WHERE ip.base_path <> '$VAULT_BASE'
-ORDER BY ip.last_modified ASC;
+    ip.mime_type,
+    ip.length as size_bytes,
+    ROUND(EXTRACT(EPOCH FROM (i_vault.last_modified - ip.last_modified)) / 86400.0, 6) AS delta_days,
+    i_vault.id as vault_id,
+    ip.id as primary_id,
+    ip.last_modified as primary_last_modified,
+    i_vault.last_modified as vault_last_modified,
+    '"' || ip.full_path || '"' AS primary_full_path,
+    '"' || i_vault.full_path || '"' AS vault_full_path
+FROM
+    images_primary ip
+        JOIN vault_hashes vh ON ip.hash = vh.hash
+        JOIN images i_vault
+             ON i_vault.hash = vh.hash
+                 AND i_vault.base_path = '$VAULT_BASE'
+WHERE
+    ip.base_path <> '$VAULT_BASE'
+  AND (i_vault.last_modified - ip.last_modified) > interval '0'
+ORDER BY
+    EXTRACT(EPOCH FROM (i_vault.last_modified - ip.last_modified))
+;
 EOF
 )
 
 echo "Searching for primary images not in vault: $VAULT_BASE"
 echo "----------------------------------------------------------------"
 
-psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER" -c "$SQL_QUERY"
+psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER" -c "$SQL_QUERY"s
