6636be95982f2844fb1efe0fe378bb4fe0d1ab2b
Tyler Johnson <tyler@netgizmo.net>
2025-12-24 15:25:57 -0600

    update last modified timestamps

--- bin/view-media-by-id.bash
+++ bin/view-media-by-id.bash
@@ -40,14 +40,26 @@ for ID in "$@"; do
             --tuples-only \
             --no-align \
             --field-separator='|' \
-            --command="SELECT full_path, mime_type FROM hashes WHERE id = $ID;")
+            --command="SELECT full_path, mime_type, 
+                CASE
+                    WHEN file_size < 1024 THEN file_size::text || ' B'
+                    WHEN file_size < 1024^2 THEN trunc(file_size::numeric / 1024, 1)::text || ' KB'
+                    WHEN file_size < 1024^3 THEN trunc(file_size::numeric / 1024^2, 1)::text || ' MB'
+                    WHEN file_size < 1024^4 THEN trunc(file_size::numeric / 1024^3, 1)::text || ' GB'
+                    ELSE trunc(file_size::numeric / 1024^4, 1)::text || ' TB'
+                END,
+                to_char(last_modified, 'YYYY-MM-DD HH24:MI:SS')
+                FROM hashes WHERE id = $ID;")
 
         if [ -n "$RESULT" ]; then
             FULL_PATH=$(echo "$RESULT" | cut -d'|' -f1)
             MIME_TYPE=$(echo "$RESULT" | cut -d'|' -f2)
+            SIZE_STR=$(echo "$RESULT" | cut -d'|' -f3)
+            DATE_STR=$(echo "$RESULT" | cut -d'|' -f4)
 
             if [ -f "$FULL_PATH" ]; then
-                echo "ID $ID : $MIME_TYPE : $FULL_PATH"
+                echo "ID : $ID MIME-TYPE: $MIME_TYPE SIZE: $SIZE_STR DATE: $DATE_STR"
+                echo "$FULL_PATH"
                 # 2. Execute imgcat
                 imgcat --preserve-aspect-ratio --width 30% "$FULL_PATH"
                 echo "" # Output a blank line
